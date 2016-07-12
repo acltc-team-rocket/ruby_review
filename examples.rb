8297df05874c3b31f96f91fd05e95b44b8320c58
Mark Richardson <markeban@yahoo.com>
2016-07-11 20:35:37 -0700

    initial commit

---
+++ examples.rb
@@ -0,0 +1,22 @@
+employee = ["Mr.", "Bob", "Bobby", "Jackson", 40000, true]
+
+puts "last name is #{employee[2]}"
+
+employee_1 = {surname: "Mr.", first_name: "Bob", last_name: "Jackson"}
+employee_2 = {surname: "Ms.", first_name: "Sally", last_name: "Clearly"}
+
+
+
+
+
+
+
+employee_hash = {:surname => "Mr.", :first_name => "Bob", :last_name => "Jackson"}
+
+
+
+
+
+# employee_hash = {"surname" => "Mr.", "first_name" => "Bob", "last_name" => "Jackson"}
+
+
