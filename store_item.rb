969b404e3585c41d0945f38ae69afd0141cebc7f
Mark Richardson <markeban@yahoo.com>
2016-07-12 19:43:27 -0700

    finished bank account exercise

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,29 @@
-pet_1 = {type: "fish", price: 3, available: true}
-pet_2 = {type: "cat", price: 2000, available: false}
-pet_3 = {type: "dog", price: 2000, available: true}
+# pet_1 = {type: "fish", price: 3, available: true}
+# pet_2 = {type: "cat", price: 2000, available: false}
+# pet_3 = {type: "dog", price: 2000, available: true}
 
-p pet_3[:price]
+# p pet_3[:price]
+
+class Pet
+
+  def initialize(pet_hash)
+    @type = pet_hash[:type]
+    @price = pet_hash[:price]
+    @availability = pet_hash[:availability]
+  end
+
+  def price
+    @price
+  end
+
+
+
+
+end
+
+
+pet = Pet.new({type: "fish", price: 3, available: true})
+cat = Pet.new({type: "cat", price: 2000, available: false})
+
+puts pet.price
+puts cat.price
